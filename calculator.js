0c25fe3a45547582a65b6abd10c3d8561d9fb245
alanb33 <ab33@protonmail.com>
2024-11-25 20:04:45 -0500

    Additional formula; code cleanup

--- calculator.js
+++ calculator.js
@@ -13,14 +13,18 @@ function calculateResults(event) {
             × (Enchant + Intelligence/5 + Luck/10 - 3×"Enchantment points")
     */
 
-    if (isAttributeInputValid()) {
+    const values = gatherInputValues();
 
-        const values = gatherInputValues()
+    if (values) {
+
         const enchantmentPointsComponent = document.getElementById("enchantment-points-input").value * 3;
-
         const finalCalculation = getFinalCalculation(values, enchantmentPointsComponent)
 
-        displayText(`The success chance is...\n${finalCalculation.toFixed(2)}%!`, "green");
+        if (finalCalculation !== null) {
+            displayText(`The success chance is...\n${finalCalculation.toFixed(2)}%!`, "green");
+        } else {
+            displayCalculationFailure();
+        }
 
     } else {
         displayText("Please fill all the fields!", "red")
@@ -28,18 +32,24 @@ function calculateResults(event) {
     
 }
 
-function displayMinSuccessChance(forMinPercentage) {
+function displayCalculationFailure() {
+    displayText("finalCalculation returned an invalid value; please let me know!", "red");
+}
+
+function displayMinSuccessChance(forMinPercentage=0) {
     console.log("Displaying min success chance >= " + forMinPercentage);
 
-    if (isAttributeInputValid())
+    const values = gatherInputValues()
+    if (values)
     {
-
-        // The maximum enchant points usability in the base game is 255, so
-        // that's going to be the cap.
+        /* 
+            The maximum enchant points usability in the base game is 
+            255, so that's going to be the cap.
+        */
         const maxEnchantability = 255;
 
         for (let enchantPoint = 1; enchantPoint <= maxEnchantability; enchantPoint++) {
-            const finalCalculation = getFinalCalculation(gatherInputValues(), enchantPoint)
+            const finalCalculation = getFinalCalculation(values, enchantPoint)
             if (enchantPoint === 1 && finalCalculation < 0) {
                 displayText("With these stats, you cannot reach a success chance of above 0%.", "red");
                 break;
@@ -58,27 +68,40 @@ function displayMinSuccessChance(forMinPercentage) {
     }
 }
 
-function displayMaxSuccessChance(forMaxPercentage) {
-    console.log("Displaying max success chance >= " + forMaxPercentage);
+function displayMaxSuccessChance(forMaxPercentage=100) {
+    /*
+        Uses a for loop to constantly update the maxPoints variable,
+        which indicates the maximum amount of points usable for the
+        given max percentage chance. Requires that Enchant,
+        Intelligence, and Luck be populated.
+    */
 
-    if (isAttributeInputValid())
+    const values = gatherInputValues();
+    if (values)
     {
         // The maximum enchant points usability in the base game is 255, so
         // that's going to be the cap.
         const maxEnchantability = 255;
-        let maxPoints = 1;
+        let maxPoints = 0;
 
         for (let enchantPoint = 1; enchantPoint <= maxEnchantability; enchantPoint++) {
             const finalCalculation = getFinalCalculation(gatherInputValues(), enchantPoint * 3)
-            if (finalCalculation < forMaxPercentage) {
-                displayText(`The maximum enchantment points for ${forMaxPercentage}% can be reached at ${maxPoints} enchant points!`, "darkgreen");
-                break;
+            if (finalCalculation) {
+                if (finalCalculation >= forMaxPercentage)
+                {
+                    maxPoints = enchantPoint;
+                } else {
+                    if (maxPoints > 0) {
+                        displayText(`The maximum enchantment points for ${forMaxPercentage}% can be reached at ${maxPoints} enchant points!`, "green");
+                        break;
+                    } else {
+                        displayText(`It is impossible to reach the goal of ${forMaxPercentage}% with your stats.`, "red");
+                        break;
+                    }
+                }
+            } else {
+                displayCalculationFailure();
             }
-            if (maxPoints == 0) {
-                displayText(`It is impossible to reach the goal of ${forMaxPercentage}% with your stats.`, "red");
-                break;
-            }
-            maxPoints = enchantPoint;
         }
     } else {
         displayText("Please fill in your Enchant skill, Intelligence, and Luck!", "red");
@@ -98,14 +121,57 @@ function getFinalCalculation(values, enchantmentPoints) {
         can call with a zero), return the final calculation as a float.
     */
 
-    const fatigueComponent = 0.75 + (1 / 2);
-    const constantComponent = values.constantEffect ? 0.5 : 1;
-    const skillComponent = values.enchant 
-                            + (values.intelligence / 5) 
-                            + (values.luck / 10);
+    const formula = getFormula();
 
-    const finalCalculation = fatigueComponent * constantComponent
-                                * (skillComponent - enchantmentPoints)
+    let finalCalculation = null;
+
+    switch (formula) {
+        case "openmw":
+            {
+                /*
+                    OpenMW's formula is:
+                    
+                    %Success = (0.75 + %Fatigue/2) 
+                    × (1-0.5×"Effect is constant") 
+                    × (Enchant + Intelligence/5 + Luck/10 - 3×"Enchantment points")
+                */
+
+                const fatigueComponent = 0.75 + (1 / 2);
+                const constantComponent = values.constantEffect ? 0.5 : 1;
+                const skillComponent = values.enchant 
+                                        + (values.intelligence / 5) 
+                                        + (values.luck / 10);
+
+                finalCalculation = fatigueComponent * constantComponent
+                                    * (skillComponent - enchantmentPoints);
+                break;
+            }
+        case "vanilla":
+        default:
+            {
+                /*
+                    Vanilla's formula is:
+
+                    %Success = (8×Enchant 
+                    + 2×Intelligence 
+                    + Luck 
+                    - 20×"Enchantment points"
+                    ×(1 + "Effect is constant"))
+                    /8
+                */
+
+                const enchant = 8 * values.enchant;
+                const intelligence = 2 * values.intelligence;
+                const luck = values.luck;
+                const enchantmentValue = 20 * enchantmentPoints;
+                const constantFactor = values.constantEffect ? 2 : 1;
+
+                finalCalculation = (enchant + intelligence + luck
+                                    - (enchantmentValue * constantFactor))
+                                    / 8;
+                break;
+            }
+        }
 
     return finalCalculation;
 
@@ -116,29 +182,44 @@ function isAttributeInputValid() {
     const intelligence = document.getElementById("intelligence-input").value;
     const luck = document.getElementById("luck-input").value;
 
-    if (!enchantSkill || !intelligence || !luck) {
-        return false;
-    } else {
-        return true;
-    }
+    return (!enchantSkill || !intelligence || !luck) ? false : true;
+}
+
+function getFormula() {
+    /*
+        If OpenMW Formula is checked, return the string "openmw". If
+        not, we return the string "vanilla". No support for any mod
+        formulas here.
+    */
+    const openMWRadioChecked = document.getElementById("openmw-formula").checked
+    return openMWRadioChecked ? "openmw" : "vanilla"
 }
 
 function gatherInputValues() {
-    // Gather all input values and result them as an object.
-    const enchantSkill = document.getElementById("enchant-input").value;
-    const intelligence = document.getElementById("intelligence-input").value;
-    const luck = document.getElementById("luck-input").value;
+    /*
+        Return values as an object if they are valid, otherwise return
+        null.
+    */
 
-    // Checkboxes return true or false
-    const constantEffect = document.getElementById("constant-effect-checkbox").checked;
+    let values = null;
 
-    console.log("Constant component: " + constantEffect);
+    if (isAttributeInputValid()) {
 
-    const values = {
-        enchant: parseFloat(enchantSkill),
-        intelligence: parseFloat(intelligence),
-        luck: parseFloat(luck),
-        constantEffect: constantEffect,
+        // Gather all input values and result them as an object.
+        const enchantSkill = document.getElementById("enchant-input").value;
+        const intelligence = document.getElementById("intelligence-input").value;
+        const luck = document.getElementById("luck-input").value;
+
+        // Checkboxes return true or false
+        const constantEffect = document.getElementById("constant-effect-checkbox").checked;
+
+        values = {
+            enchant: parseFloat(enchantSkill),
+            intelligence: parseFloat(intelligence),
+            luck: parseFloat(luck),
+            constantEffect: constantEffect,
+        }
+
     }
 
     return values;
